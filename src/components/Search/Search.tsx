4eb7bb33b95908f95ab53fb6d5475c269874cc0f
Narek <narekazibekyan5@gmail.com>
2023-10-03 18:11:24 +0400

    Fixed bug in the input field

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
 import { getSearchMoviesOrTvsOrPersonsData } from "../../services/asyncActions";
@@ -20,6 +20,20 @@ interface SearchProps {
   setSearchValue: (e: React.SetStateAction<string>) => void;
 }
 
+function useDebounce(value: string, delay?: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
+
 const Search: FC<SearchProps> = ({
   dispatchType,
   searchValue,
@@ -27,70 +41,79 @@ const Search: FC<SearchProps> = ({
 }) => {
   const dispatch = useDispatch();
 
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    switch (dispatchType) {
+      case GET_SEARCHABLE_MOVIES:
+        localStorage.setItem("searchableMoviesValue", e.target.value);
+        dispatch({ type: DELETE_ALL_MOVIE_GENRES });
+        dispatch(
+          // @ts-ignore
+          getSearchMoviesOrTvsOrPersonsData(
+            dispatchType,
+            e.target.value,
+            "movie",
+            1
+          )
+        );
+        break;
+
+      case GET_SEARCHABLE_TVS:
+        localStorage.setItem("searchableTvsValue", e.target.value);
+        dispatch({ type: DELETE_ALL_TV_GENRES });
+        dispatch(
+          // @ts-ignore
+          getSearchMoviesOrTvsOrPersonsData(
+            dispatchType,
+            e.target.value,
+            "tv",
+            1
+          )
+        );
+        break;
+
+      case GET_SEARCHABLE_MULTI:
+        localStorage.setItem("searchableMultiValue", e.target.value);
+        dispatch(
+          // @ts-ignore
+          getSearchMultiData(dispatchType, e.target.value, 1)
+        );
+        break;
+
+      case GET_SEARCHABLE_PERSONS:
+        localStorage.setItem("searchablePersonsValue", e.target.value);
+        dispatch(
+          // @ts-ignore
+          getSearchMoviesOrTvsOrPersonsData(
+            dispatchType,
+            e.target.value,
+            "person",
+            1
+          )
+        );
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className={styles.search}>
       <input
         className={styles.search__input}
         value={searchValue}
-        onChange={(e) => {
-          setSearchValue(e.target.value);
-
-          switch (dispatchType) {
-            case GET_SEARCHABLE_MOVIES:
-              localStorage.setItem("searchableMoviesValue", e.target.value);
-              dispatch({ type: DELETE_ALL_MOVIE_GENRES });
-              dispatch(
-                // @ts-ignore
-                getSearchMoviesOrTvsOrPersonsData(
-                  dispatchType,
-                  e.target.value,
-                  "movie",
-                  1
-                )
-              );
-              break;
-
-            case GET_SEARCHABLE_TVS:
-              localStorage.setItem("searchableTvsValue", e.target.value);
-              dispatch({ type: DELETE_ALL_TV_GENRES });
-              dispatch(
-                // @ts-ignore
-                getSearchMoviesOrTvsOrPersonsData(
-                  dispatchType,
-                  e.target.value,
-                  "tv",
-                  1
-                )
-              );
-              break;
-
-            case GET_SEARCHABLE_MULTI:
-              localStorage.setItem("searchableMultiValue", e.target.value);
-              dispatch(
-                // @ts-ignore
-                getSearchMultiData(dispatchType, e.target.value, 1)
-              );
-              break;
-
-            case GET_SEARCHABLE_PERSONS:
-              localStorage.setItem("searchablePersonsValue", e.target.value);
-              dispatch(
-                // @ts-ignore
-                getSearchMoviesOrTvsOrPersonsData(
-                  dispatchType,
-                  e.target.value,
-                  "person",
-                  1
-                )
-              );
-              break;
-
-            default:
-              break;
-          }
-        }}
+        onChange={handleSearch}
         type="text"
-        placeholder="Search Movie"
+        placeholder={`Search ${
+          dispatchType.includes("MULTI")
+            ? ""
+            : dispatchType.includes("MOVIE")
+            ? "Movie"
+            : dispatchType.includes("PERSON")
+            ? "Person"
+            : "Tv"
+        }`}
       />
     </div>
   );
